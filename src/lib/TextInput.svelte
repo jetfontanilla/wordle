73b8c7c1b6b6c9478aa50d8e2335dbe2dd7cf14e
Jet Fontanilla <jetlogs@gmail.com>
2022-01-24 15:59:45 +0900

    text input navigation and focus

--- src/lib/TextInput.svelte
+++ src/lib/TextInput.svelte
@@ -2,20 +2,50 @@
     import { Attempt } from '../routes/wordle/attempt';
     import { createEventDispatcher } from 'svelte';
 
+    const ARROW_LEFT = 'ArrowLeft';
+    const ARROW_RIGHT = 'ArrowRight';
+
     export let attempt: Attempt;
     let inputFields = [];
 
     const dispatch = createEventDispatcher();
 
-    function onClear(index) {
+    function onClear(index, letter) {
+        if (letter == ARROW_LEFT || letter == ARROW_RIGHT) {
+            return;
+        }
         inputFields[index].value = "";
     }
 
     function onChange(index, letter) {
+        if (letter == ARROW_LEFT) {
+            return moveLeft(index)
+        }
+        if (letter == ARROW_RIGHT) {
+            return moveRight(index)
+        }
+        if (!/[a-z]/i.test(letter)) {
+            console.log(letter);
+            return;
+        }
         dispatch('stateChange', {
             index: index,
             letter: letter
         });
+
+        moveRight(index);
+    }
+
+    function moveRight(index) {
+        if (inputFields[index + 1]) {
+            inputFields[index + 1].focus();
+        }
+    }
+
+    function moveLeft(index) {
+        if (inputFields[index - 1]) {
+            inputFields[index - 1].focus();
+        }
     }
 </script>
 
@@ -29,7 +59,7 @@
                bind:this={inputFields[index]}
                value={letterState.value}
                readonly={attempt.readonly}
-               on:keydown={e => onClear(index)}
+               on:keydown={e => onClear(index, e.key)}
                on:keyup={e => onChange(index, e.key)}
         />
     {/each}
