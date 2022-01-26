13fdcede21be91601c93770ad08c908c9b2023a7
Jet Fontanilla <jetlogs@gmail.com>
2022-01-26 16:06:56 +0900

    GitHub Pages build config

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
+
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,6 +11,12 @@ const config = {
 
 	kit: {
 		adapter: adapter(),
+
+		paths: {
+			base: dev ? '' : '/wordle',
+		},
+
+		appDir: 'internal',
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
